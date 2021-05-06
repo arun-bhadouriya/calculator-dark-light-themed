a273c5fe42fa1937595f8bfab166a06df6df4c9a
thakur00 <arunbhadouriya71@gmail.com>
2021-05-06 13:21:26 +0530

    update for history v1

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const numbers = document.getElementsByClassName("number");
+
 const lowerValue = document.getElementById("lower-value");
 const upperValue = document.getElementById("upper-value");
 const operator = document.getElementsByClassName("operator");
 let checkbox = document.querySelector('input[name="theme"]');
 let toggleTheme = document.querySelector(".fas.fa-moon");
+let expand = document.querySelector(
+	"body > div > div.heading > span:nth-child(1) > i"
+);
+let expEl = document.querySelector("body > div > div.expand");
+
+//array to capture history records
+let records = localStorage.getItem("record")
+	? JSON.parse(localStorage.getItem("record"))
+	: [];
 
 toggleTheme.addEventListener("click", () => {
 	checkbox.checked = !checkbox.checked;
@@ -54,20 +64,85 @@ for (let i = 0; i < operator.length; i++) {
 }
 
 function evaluate(x, y, operator) {
+	let record = { firstParam: x, lastParam: y, operator: operator };
+
 	if (operator == "*") {
 		upperValue.innerText = "";
 		lowerValue.innerText = x * y;
+		record.answer = x * y;
 	} else if (operator == "/") {
 		upperValue.innerText = "";
 		lowerValue.innerText = x / y;
+		record.answer = x / y;
 	} else if (operator == "+") {
 		upperValue.innerText = "";
 		lowerValue.innerText = x + y;
+		record.answer = x + y;
 	} else if (operator == "-") {
 		upperValue.innerText = "";
 		lowerValue.innerText = x - y;
+		record.answer = x - y;
 	} else if (operator == "%") {
 		upperValue.innerText = "";
 		lowerValue.innerText = x % y;
+		record.answer = x % y;
 	}
+
+	records.push(record);
+
+	let expandData = "";
+	records.forEach((e) => {
+		expandData += `<div class="record">
+					
+					<span class="query">${
+						e.firstParam + e.operator + e.lastParam
+					}<span><i class="fas fa-trash-alt"></i></span></span>
+					<span class="ans">= ${e.answer}</span>
+				</div>`;
+	});
+	expEl.innerHTML = expandData;
+
+	localStorage.setItem("record", JSON.stringify(records));
+
+	const removeEl = document.querySelectorAll(
+		"body > div > div.expand > div.record > span.query > span"
+	);
+	removeEl.forEach((element) => {
+		element.addEventListener("click", (e) => {
+			console.log("clicked removeEl");
+			element.parentElement.parentElement.remove();
+		});
+	});
+	console.log(removeEl);
 }
+
+//expand
+let count = 0;
+expand.addEventListener("click", () => {
+	console.log("click");
+	const d = JSON.parse(localStorage.getItem("record"));
+	console.log(d);
+	let expandData = "";
+	d.forEach((e) => {
+		expandData += `<div class="record">
+					
+					<span class="query">${
+						e.firstParam + e.operator + e.lastParam
+					}<span><i class="fas fa-trash-alt"></i></span></span>
+					<span class="ans">= ${e.answer}</span>
+				</div>`;
+	});
+	expEl.innerHTML = expandData;
+	const removeEl = document.querySelectorAll(
+		"body > div > div.expand > div.record > span.query > span"
+	);
+	removeEl.forEach((element) => {
+		element.addEventListener("click", (e) => {
+			console.log("clicked removeEl");
+			element.parentElement.parentElement.remove();
+		});
+	});
+	if (records.length) {
+		expEl.classList.toggle("show");
+	}
+});
